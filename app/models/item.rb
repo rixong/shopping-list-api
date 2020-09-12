19768b0dd94e1792c64515c17e0a4de5fb978e2b
rixong <rixong@gmail.com>
2020-09-12 16:25:22 -0500

    added associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  belongs_to :list_items
+  belongs_to :lists, through: :list_items
+  belongs_to :categories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  
+  has_many :lists, dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :categories
 end
