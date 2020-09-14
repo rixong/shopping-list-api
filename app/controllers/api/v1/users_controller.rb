500d4b2a7d28e83652f823a112433838307b470a
rixong <rixong@gmail.com>
2020-09-14 15:03:28 -0500

    user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,10 +7,11 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     user = User.find(params[:id])
-    cur_list = user.lists.order(created_at: :desc).limit(1)
+    cur_list = user.lists.order(created_at: :desc).limit(1)[0]
+    cur_list_items = cur_list.list_items
     categories = user.categories
     items = user.items
-    render json: {user: user, curList: cur_list, items: items, categories: categories}
+    render json: {user: user, curList: cur_list, items: items, curListItems: cur_list_items, categories: categories}
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2020_09_12_211541) do
 
   create_table "list_items", force: :cascade do |t|
     t.string "quantity"
-    t.boolean "active"
+    t.boolean "active", default: true
     t.integer "list_id"
     t.integer "item_id"
     t.datetime "created_at", precision: 6, null: false
