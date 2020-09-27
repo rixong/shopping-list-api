d9c1ce13fb46cd91a15caf71464f90e542548be1
rixong <rixong@gmail.com>
2020-09-27 18:05:54 -0500

    shut down users index endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
-  skip_before_action :authorized, only: [:create, :index]
+  skip_before_action :authorized, only: [:create]
 
 
   def profile
